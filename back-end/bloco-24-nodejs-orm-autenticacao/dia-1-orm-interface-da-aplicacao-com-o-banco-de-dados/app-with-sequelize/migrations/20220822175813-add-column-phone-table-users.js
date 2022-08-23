51f2acaa8ef7375835fc0f7edb7b4c33d93ca711
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-08-23 10:46:02 -0300

    Sequelize

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/migrations/20220822175813-add-column-phone-table-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add altering commands here.
+//      *
+//      * Example:
+//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//      */
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add reverting commands here.
+//      *
+//      * Example:
+//      * await queryInterface.dropTable('users');
+//      */
+//   }
+// };
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.addColumn('Users', 'phone_num', {
+     type: Sequelize.STRING,
+   });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'phone_num');
+  }
+};

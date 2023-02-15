8548a5a1fbcc9efd98235cdd29b2a91a1e05e3e9
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-02-15 11:05:17 -0300

    feat(exercicios): exercicios dia 2 python

---
+++ python/desafio10.py
@@ -0,0 +1,5 @@
+meu_dict = {123: "um dois tres", "chave": "valor"}
+
+print(meu_dict[123])
+# print(meu_dict[chave]) com erro 
+print(meu_dict["chave"])
---
+++ python/dia2_exercicios.py
@@ -0,0 +1,32 @@
+nums = input("Insira valores aqui, separados por espaço: ")
+
+numsArr = nums.split(" ")
+
+sum = 0
+for num in numsArr:
+    if not num.isdigit():
+        print(f"Erro ao somar valores, {num} não é um dígito.")
+    else:
+        sum += int(num)
+
+print(f"A soma dos valores válidos é: {sum}")
+
+name_user = input("Digite seu Nome: ");
+
+
+for letters in name_user:
+    print(letters)
+
+
+recuStudents = []
+with open("file.txt") as gradesFile:
+    for line in gradesFile:
+        student_grade = line
+        student_grade = student_grade.split(" ")
+        if int(student_grade[1]) < 6:
+            recuStudents.append(student_grade[0] + "\n")
+
+
+with open("recuStudents.txt", mode="w") as recuStudentsFile:
+    print(recuStudents)
+    recuStudentsFile.writelines(recuStudents)
